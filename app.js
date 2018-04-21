a29bedfe5685fb538a507170107da9e3b63b9072
mc2147 <matthewchan2147@gmail.com>
2018-04-21 11:30:39 -0500

    cors fixed

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ var allowCrossDomain = function(req, res, next) {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-        res.send(200);
+        // res.send(200);
         next();
     // }
     // else {
