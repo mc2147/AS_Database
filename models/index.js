a33ee21119a53e4bc42e8fbacb42fa7599886932
mc2147 <matthewchan2147@gmail.com>
2018-04-21 09:51:22 -0500

    database url added

--- models/index.js
+++ models/index.js
@@ -11,7 +11,7 @@ const {Alloy} = require('../globals/enums');
 	// var ExerciseDict = data.ExerciseDict.Exercises;
 
 
-const db = new Sequelize('postgres://localhost:5432/AS_db', {
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/AS_db', {
     logging: false,
     dialectOptions: { decimalNumbers: true }
 });
